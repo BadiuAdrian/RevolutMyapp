68f12aed0f9ff50cc57f76506d88cb045ee1c80b
BadiuAdrian <128799896+BadiuAdrian@users.noreply.github.com>
2023-03-24 18:43:44 +0200

    implement spinner

---
+++ app/src/main/java/com/example/aplicatie_revolut/GroceriesType.java
@@ -0,0 +1,18 @@
+package com.example.aplicatie_revolut;
+
+
+public enum GroceriesType {
+    Food, Drinks;
+//    Food("food"), Drinks("drinks");
+//    private String spinnerName;
+//    private GroceriesType(String value){
+//        spinnerName=value;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public String toString() {
+//        return spinnerName;
+//    }
+}
+
